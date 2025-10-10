0115afc67629d3abd8a2b61781c6eea1bb769a87
xaviersanc <33207785+xaviersanc@users.noreply.github.com>
2025-10-10 10:01:46 +0200

    Feat2 : Ajout page de connexion

--- src/Controller/ListeVMController.php
+++ src/Controller/ListeVMController.php
@@ -8,11 +8,39 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ListeVMController extends AbstractController
 {
+
+        #[Route('/connection', name: 'app_connection')]
+    public function connection(): Response
+    {
+        return $this->render('connection.html.twig', [
+            'controller_name' => 'ListeVMController',
+        ]);
+    }
+
+
     #[Route('/liste/v/m', name: 'app_liste_v_m')]
     public function index(): Response
     {
         return $this->render('liste_vm/index.html.twig', [
             'controller_name' => 'ListeVMController',
+        ]);
+    }
+
+    
+
+        #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('inscription.html.twig', [
+            'controller_name' => 'ListeVMController',
+        ]);
+    }
+
+        #[Route('/forgeten_password', name: 'app_forgeten_password')]
+    public function forgeten_password(): Response
+    {
+        return $this->render('ForgetenPassword.html.twig', [
+            'controller_name' => 'ForgetenPasswordController',
         ]);
     }
 }
